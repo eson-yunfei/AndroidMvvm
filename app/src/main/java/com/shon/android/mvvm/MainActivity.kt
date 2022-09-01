d19ef4f3e7bd31099c56be82e8d1dc577a088549
xiaoyunfei <xiao_yf@hisuntech.com>
2022-09-01 17:45:27 +0800

    删除一些旧的代码

--- app/src/main/java/com/shon/android/mvvm/MainActivity.kt
+++ app/src/main/java/com/shon/android/mvvm/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.shon.android.mvvm
 
-import android.util.Log
+import android.content.Intent
+import android.view.View
 import com.shon.android.mvvm.databinding.ActivityMainBinding
 import com.shon.mvvm.BaseViewModel
 import com.shon.mvvm.activity.BaseVmDbActivity
 
-class MainActivity : BaseVmDbActivity<BaseViewModel,ActivityMainBinding>(){
-
-     class ClickProxy{
-
-        fun onSingeExtent(){
-            Log.d("MainActivity", "onSingeExtent: ")
-        }
-    }
+class MainActivity : BaseVmDbActivity<BaseViewModel, ActivityMainBinding>() {
 
     override fun onInitData() {
         dataBinding.mainModel = ClickProxy()
     }
+
     override fun getDataBindingLayoutId(): Int {
         return R.layout.activity_main
+    }
+
+
+    internal class ClickProxy {
+
+        fun onSingeExtent(view:View) {
+            view.context.startActivity(Intent(view.context,ViewBindingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/shon/android/mvvm/ViewBindingActivity.kt
@@ -0,0 +1,9 @@
+package com.shon.android.mvvm
+
+import com.shon.android.mvvm.databinding.ActivityViewBindingBinding
+import com.shon.mvvm.BaseViewModel
+import com.shon.mvvm.activity.BaseVmVbActivity
+
+class ViewBindingActivity : BaseVmVbActivity<BaseViewModel,ActivityViewBindingBinding>() {
+
+}
