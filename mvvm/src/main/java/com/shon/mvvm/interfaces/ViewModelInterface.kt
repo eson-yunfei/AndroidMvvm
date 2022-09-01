273135546e4b8e80c4ffff56f72d56926aeb2e58
xiao_yf <xiao_yf@hisuntech.com>
2022-09-01 21:31:26 +0800

    删除旧代码

--- mvvm/src/main/java/com/shon/mvvm/interfaces/ViewModelInterface.kt
+++ mvvm/src/main/java/com/shon/mvvm/interfaces/ViewModelInterface.kt
@@ -6,13 +6,16 @@ import com.shon.mvvm.BaseViewModel
 import com.shon.mvvm.GetObjects
 
 /**
- *
+ *  ViewModel 的实现接口
  * @Author xiao
  * @Date 2022-09-01 16:18
  *
  */
 internal interface ViewModelInterface<VM : BaseViewModel> {
 
+    /**
+     * 创建 ViewModel 并且绑定 ViewModel
+     */
     fun onAttachView(lifecycleOwner: LifecycleOwner, factory: ViewModelProvider.Factory): VM {
 
         val viewMode = factory.create<VM>(GetObjects.getVMClazz(this))
